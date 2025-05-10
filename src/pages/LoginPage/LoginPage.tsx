8ca42b5a2820d7a418e6f80eee9d115fd9e7ca6c
leandromonta13 <leojaja587@gmail.com>
2025-05-09 21:44:08 -0300

    loginsucesso

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,31 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './LoginPage.module.css';
 
-const LoginPage: React.FC = () => {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+
+interface LoginProps {
+  onLoginSuccess?: (toKen: string) => void;
+}
+
+
+
+const LoginPage: React.FC<LoginProps> = ({ onLoginSuccess }) => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
+  
+
+
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
-    // Aqui você pode adicionar a lógica de autenticação
-    console.log("Formulário enviado");
+    setError (null);
+    try {
+      const res = await fetch(
+        "https://senac-eventos-cultural-backend-production.up.railway.app/auth/login",
+      {
+        method: "POST",
+        headers: { "content-Type": "application/json" },
+        body: JSON.stringify({ email, password}),
+
+      }
+
+    );
+    if (!res.ok) {
+      const data = await res.json();
+      throw new Error(data.message || "Falha no login");
+    }
+    const data = await res.json();
+    onLoginSuccess?.(data.toKen);
+    alert("Login Efetuado");
+
+
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        setError(err.message);
+        alert('Erro ao logar: ${err.message}');
+        
+      } else {
+        const errorMsg = String(err);
+        setError(errorMsg);
+        alert('Erro ao logar: ${errorMsg}');
+
+
+      }
+
+    }
+
   };
+
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Tela de Login</h1>
       <form onSubmit={handleSubmit} className={styles.form}>
-        <label htmlFor="email" className={styles.label}>Email</label>
+
+        {error && <div>{error}</div>}
+
+        <label htmlFor="email" className={styles.label}>
+          Email
+          </label>
         <input
           type="email"
           id="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
           name="email"
           className={styles.input}
           placeholder="Digite seu email"
           required
         />
 
-        <label htmlFor="password" className={styles.label}>Senha</label>
+        <label htmlFor="password" className={styles.label}>
+          Senha
+          </label>
         <input
           type="password"
           id="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
           name="password"
           className={styles.input}
           placeholder="Digite sua senha"
